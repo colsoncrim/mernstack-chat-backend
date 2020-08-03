1101f8cc9e836a224f600423e6068246f446f57f
colsoncrim <colsonscrim@gmail.com>
2020-08-02 22:17:54 -0700

    Private routes for deleting users

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth");
 
 //1. REGISTER A USER
 router.post("/register", async (req, res) => {
@@ -83,6 +84,35 @@ router.post("/login", async (req, res) => {
         });
 
     }     catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+//*************************************************** */
+//PRIVATE ROUTES (must be logged in to perform these operations)
+router.delete("/delete", auth, async (req, res) => {
+    try {
+        const deletedUser = await User.findByIdAndDelete(req.user);
+        res.json(deletedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+});
+
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+        const token = req.header("x-auth-token");
+        if (!token) return res.json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified) return res.json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user) return res.json(false);
+
+        return res.json(true);
+
+    } catch (err) {
         res.status(500).json({ error: err.message });
     }
 });
